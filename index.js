853f37a4e4d26ff850c684739974847589521791
hank <hank8451@gmail.com>
2020-08-25 16:55:39 +0800

    task_3 finish

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-  document.querySelector('#navbar-burger').addEventListener('click', ()=>{
-    
+  const jobPannel = document.querySelector('#job-pannel')
+  let itemTemp= document.querySelector('#itemTemp')
+  let total = []
+  
+  // console.log(template)
+  document.querySelector('#navbar-burger').addEventListener('click', (event)=>{
+    event.preventDefault()
     const menu = document.querySelector('#navbar-menu');
     if (menu.classList.contains('is-active')){
       menu.classList.remove('is-active')
@@ -17,10 +22,37 @@ document.addEventListener('DOMContentLoaded', ()=>{
     // console.log(jobFullTime)
     if (!!jobDes || !!jobLoc || jobFullTime){
       fetch(`https://still-spire-37210.herokuapp.com/positions.json?description=${jobDes}&full_time=${jobFullTime}&location=${jobLoc}`)
-      .then(response => response.json())
+      .then(response => response.json()) 
       .then(searchData => {
         console.log(searchData)
+        jobPannel.innerHTML="";
+        searchData.forEach(element => {
+          let result={
+            jobTitle: element["title"],
+            jobUrl: element["url"],
+            jobCompany: element["company"],
+            jobCompanyUrl: element["company_url"],
+            jobType: element["type"],
+            jobLocation: element["location"]
+          };
+          jobPannel.appendChild(createRecord(result))
+        });
       })
     }   
  })
+
+  function createRecord({jobTitle, jobUrl, jobCompany, jobCompanyUrl, jobType, jobLocation}){
+    const h4 = itemTemp.content.querySelector("h4")
+    const h4a = itemTemp.content.querySelector("h4 > a")
+    const company = itemTemp.content.querySelector(".company")
+    const fulltime = itemTemp.content.querySelector(".fulltime")
+    const location = itemTemp.content.querySelector(".location")
+    h4.setAttribute("href", jobUrl)
+    h4a.textContent = jobTitle
+    company.setAttribute("href", jobCompanyUrl)
+    company.textContent = jobCompany
+    fulltime.textContent = jobType
+    location.textContent = jobLocation
+    return document.importNode(itemTemp.content, true)
+  }
 })
